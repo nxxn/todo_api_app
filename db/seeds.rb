0f69f3862547f59ebe7a203424ac40b352cc8494
gnotov <nikita@gnotov.com>
2019-05-13 23:43:44 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tag.create(title: "Today")
+Task.create(title: "Wash laundry", tags: Tag.all)
